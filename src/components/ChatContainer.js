cf0542630ff1c9ee9bb5189ac904209436e7d11d
komus-Israel <komolehinIsrael@gmail.com>
2021-11-12 19:52:13 +0100

    added placeholder to the typemessage div

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -9,8 +9,8 @@ const ChatContainer=({message, setMessage, handleChat, chats})=>{
                         <Messages msgs={chats}/>    
                     </div>
                     <div className='type-message'>
-                       <textarea value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
-                        <button className='send-chat' onClick={()=>message.length > 0 && handleChat(message)}>send</button>
+                       <textarea placeholder='message' value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
+                        <p className='send-chat' onClick={()=>message.length > 0 && handleChat(message)}>send</p>
                        
                     </div>
                     
